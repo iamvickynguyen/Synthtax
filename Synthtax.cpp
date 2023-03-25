4337b7a2647d65d2c23f36b4cb0e84cf1b3d95bb
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-03-24 23:16:01 -0700

    clang-format

--- Synthtax.cpp
+++ Synthtax.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
-
 #include "antlr4-runtime.h"
 #include "libs/SynthtaxLexer.h"
 #include "libs/SynthtaxParser.h"
 
 using namespace antlr4;
 
-int main(int argc, const char* argv[]) {
-    std::ifstream stream;
-    stream.open("test/hello.in");
-    
-    ANTLRInputStream input(stream);
-		synthtax_antlr::SynthtaxLexer lexer(&input);
-    CommonTokenStream tokens(&lexer);
-		synthtax_antlr::SynthtaxParser parser(&tokens);    
-    
-		tree::ParseTree* tree = parser.prog();
-	  std::cout << tree->toStringTree(&parser) << "\n\n";
+int main(int argc, const char *argv[]) {
+  std::ifstream stream;
+  stream.open("test/hello.in");
 
-    return 0;
+  ANTLRInputStream input(stream);
+  synthtax_antlr::SynthtaxLexer lexer(&input);
+  CommonTokenStream tokens(&lexer);
+  synthtax_antlr::SynthtaxParser parser(&tokens);
+
+  tree::ParseTree *tree = parser.prog();
+  std::cout << tree->toStringTree(&parser) << '\n';
+
+  return 0;
 }
