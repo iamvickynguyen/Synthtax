ed312fa53e85c626452f96adc80f756058275938
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-04-14 20:44:16 -0700

    can print block, add indentation

--- Visitor.h
+++ Visitor.h
@@ -11,7 +11,7 @@ class Visitor : public SynthtaxParserBaseVisitor {
 public:
 	std::stringstream outfile;
 
-  Visitor() {}
+  Visitor(): indentLevel(1) {}
 
   std::any visitProg(SynthtaxParser::ProgContext *ctx) {
 		outfile << "#include <builtin.h>\n";
@@ -20,6 +20,7 @@ public:
 		
 		for (auto &f: ctx->function()) {
 			visitFunction(f);
+			outfile << "\n";
 		}
 
 		return NULL;
@@ -36,7 +37,7 @@ public:
   std::any
   visitFunction(SynthtaxParser::FunctionContext *ctx) {
     visitFuncDeclaration(ctx->funcDeclaration());
-//		visitFuncBody(ctx->funcBody());
+		visitFuncBody(ctx->funcBody());
 		return NULL;
   }
 
@@ -49,7 +50,6 @@ public:
 			visitFormalParameters(ctx->formalParameters());
 	
 		outfile << ")";
-		outfile << '\n'; // TEMP
 		return visitChildren(ctx);
   }
 
@@ -70,40 +70,117 @@ public:
 
   std::any
   visitFuncBody(SynthtaxParser::FuncBodyContext *ctx) {
-		std::cout << "visit FuncBodyContext\n";
-    return visitChildren(ctx);
+		outfile << " {";
+
+		if (ctx->statement().size() > 0) outfile << "\n";
+		
+		for (auto &s: ctx->statement()) {
+			indent();
+			visitStatement(s);
+		}
+		
+		if (ctx->statement().size() > 0) outfile << "\n";
+		
+		outfile << "}\n";
+		return NULL;
   }
   
   std::any visitStatement(SynthtaxParser::StatementContext *ctx) {
-    return visitChildren(ctx);
-  }
+  	if (ctx->varDeclaration() != nullptr) visitVarDeclaration(ctx->varDeclaration());
+		else if (ctx->expressionStatement() != nullptr) visitExpressionStatement(ctx->expressionStatement());
+		else if (ctx->ifStatement() != nullptr) visitIfStatement(ctx->ifStatement());
+		else if (ctx->whileStatement() != nullptr) visitWhileStatement(ctx->whileStatement());
+		else if (ctx->returnStatement() != nullptr) visitReturnStatement(ctx->returnStatement());
+		else if (ctx->assignmentStatement() != nullptr) visitAssignmentStatement(ctx->assignmentStatement());
+		else outfile << ";\n";
+		return NULL;
+	}
 
   std::any visitVarDeclaration(SynthtaxParser::VarDeclarationContext *ctx) {
+		print_type(ctx->TYPE()->getText());
+		outfile << " ";
+
+		if (ctx->assignmentStatement() != nullptr) visitAssignmentStatement(ctx->assignmentStatement());
+		else outfile << ctx->ID()->getText() << ";\n";
     return visitChildren(ctx);
   }
 
   std::any visitExpressionStatement(SynthtaxParser::ExpressionStatementContext *ctx) {
-    return visitChildren(ctx);
+    return visitExpression(ctx->expression());
   }
 
   std::any visitIfStatement(SynthtaxParser::IfStatementContext *ctx) {
-    return visitChildren(ctx);
+		outfile << "if (";
+		visitExpression(ctx->expression());
+		outfile << ") {";
+		
+		if (ctx->block()[0] != nullptr) {
+			++indentLevel;
+			outfile << "\n";
+			visitBlock(ctx->block()[0]);
+			outfile << "\n";
+			--indentLevel;
+
+			indent();
+		}
+
+		outfile << "}";
+
+		if (ctx->block().size() > 1) {
+			outfile << " else {";
+			if (ctx->block()[1] != nullptr) {
+				++indentLevel;
+				outfile << "\n";
+				visitBlock(ctx->block()[1]);
+				outfile << "\n";
+				--indentLevel;
+
+				indent();
+			}
+			outfile << "}";
+		}
+
+		outfile << "\n";
+    return NULL;
   }
 
   std::any visitWhileStatement(SynthtaxParser::WhileStatementContext *ctx) {
-    return visitChildren(ctx);
+		outfile << "while (";
+		visitExpression(ctx->expression());
+		outfile << ") {";
+
+		if (ctx->block()) {
+			outfile << "\n";
+			visitBlock(ctx->block());
+			outfile << "\n";
+
+			indent();
+		}
+
+		outfile << "}\n";
+    return NULL;
   }
 
   std::any visitReturnStatement(SynthtaxParser::ReturnStatementContext *ctx) {
-    return visitChildren(ctx);
+		outfile << "return ";
+		visitExpression(ctx->expression());
+		outfile << ";\n";
+    return NULL;
   }
 
   std::any visitAssignmentStatement(SynthtaxParser::AssignmentStatementContext *ctx) {
-    return visitChildren(ctx);
+		outfile << ctx->ID()->getText() << " = ";
+		visitExpression(ctx->expression());
+		outfile << ";\n";
+    return NULL; 
   }
 
   std::any visitBlock(SynthtaxParser::BlockContext *ctx) {
-    return visitChildren(ctx);
+		for (auto &s: ctx->statement()) {
+			indent();
+			visitStatement(s);
+		}
+    return NULL;
   }
 
   std::any visitExpression(SynthtaxParser::ExpressionContext *ctx) {
@@ -135,9 +212,17 @@ public:
   }
 
 private:
+	int indentLevel;
+
 	void print_type(const std::string &type) {
 		if (type == "string") outfile << "std::string";
 		else outfile << type;
+	}
+
+	void indent() {
+		for (int i = 0; i < indentLevel; ++i) {
+			outfile << "  ";
+		}
 	}
 };
 } // namespace synthtax_antlr
