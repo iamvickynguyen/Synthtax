8b59a622ba260a2fe50014e88f97dac1154ca850
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-04-15 23:35:50 -0700

    add libraries to include folder, edit CMakeLists.txt

---
+++ include/sndfile/sndfile.h
@@ -0,0 +1,9 @@
+#include <bits/wordsize.h>
+
+#if __WORDSIZE == 32
+# include "sndfile-32.h"
+#elif __WORDSIZE == 64
+# include "sndfile-64.h"
+#else
+# error "unexpected value for __WORDSIZE macro"
+#endif
