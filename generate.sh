95ef0af6ca94a344969c2be6c7d82c28636df8bc
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-08-09 21:15:02 -0700

    rm unused

--- generate.sh
+++ generate.sh
@@ -2,6 +2,4 @@
 set -o errexit
 
 LOCATION=tool/antlr-4.12.0-complete.jar
-#java -jar $LOCATION -Dlanguage=Cpp -listener -visitor -o generated/ -package antlrcpptest TLexer.g4 TParser.g4
 java -jar $LOCATION -Dlanguage=Cpp -no-listener -visitor -o libs/ -package synthtax_antlr SynthtaxLexer.g4 SynthtaxParser.g4
-#java -jar $LOCATION -Dlanguage=Cpp -o generated/ Synthtax.g4 
