dd5f94ed86becc62301f037d71b11fd3148eb28b
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-08-15 19:22:44 -0700

    can output module to ofstream

--- Synthtax.cpp
+++ Synthtax.cpp
@@ -1,14 +1,16 @@
 #include "antlr4-runtime.h"
 #include "libs/SynthtaxLexer.h"
 #include "libs/SynthtaxParser.h"
-#include "Visitor.h"
+#include "CppTranspilerVisitor.h"
+#include "IRBuilder.h"
 #include <fstream>
 #include <iostream>
 
 using namespace antlr4;
+using namespace llvm;
 
 int main(int argc, const char *argv[]) {
-	if (argc != 2) {
+	if (argc != 3) {
 		std::cout << "Invalid argument(s)\n";
 		return 0;
 	}
@@ -20,28 +22,32 @@ int main(int argc, const char *argv[]) {
   synthtax_antlr::SynthtaxLexer lexer(&input);
   CommonTokenStream tokens(&lexer);
 
-//  tokens.fill();
-//  for (auto token : tokens.getTokens()) {
-//    std::cout << token->toString() << '\n';
-//  }
-//
-//  lexer.reset();
-
   synthtax_antlr::SynthtaxParser parser(&tokens);
-//
-// tree::ParseTree *tree = parser.prog();
-// std::cout << tree->toStringTree(&parser) << '\n';
-
 	synthtax_antlr::SynthtaxParser::ProgContext *prog = parser.prog();
 
-  synthtax_antlr::Visitor visitor;
-  visitor.visitProg(prog);
+  // synthtax_antlr::CppTranspilerVisitor visitor;
+  // visitor.visitProg(prog);
 
-	std::ofstream out;
-	out.open("../output.cpp");
-	out << visitor.outfile.str();
+  // std::ofstream out;
+  // out.open(argv[2]);
+  // out << visitor.outfile.str();
+  // out.close();
 
-	out.close();
-  stream.close();
+  llvm::LLVMContext context;
+  llvm::Module module("synthtax_module", context);
+  llvm::IRBuilder<> builder(context);
+
+  synthtax_antlr::IRBuilder ir_builder(context, module, builder);
+  ir_builder.visitProg(prog);
+
+  std::string str;
+  llvm::raw_string_ostream os(str);
+  module.print(os, nullptr);
+
+  std::ofstream out;
+  out.open(argv[2]);
+  out << str;
+  out.close();
+
   return 0;
 }
