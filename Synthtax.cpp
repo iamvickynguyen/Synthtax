14c533688d3b0fb7f7b5b4eb8e693282498445fb
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-09-12 12:27:04 -0700

    update README, edit main()

--- Synthtax.cpp
+++ Synthtax.cpp
@@ -9,6 +9,34 @@
 using namespace antlr4;
 using namespace llvm;
 
+void transpile(const char *filepath, synthtax_antlr::SynthtaxParser::ProgContext *prog) {
+  synthtax_antlr::CppTranspilerVisitor visitor;
+  visitor.visitProg(prog);
+
+  std::ofstream out;
+  out.open(filepath);
+  out << visitor.outfile.str();
+  out.close();
+}
+
+void build_ir(const char *filepath, synthtax_antlr::SynthtaxParser::ProgContext *prog) {
+  llvm::LLVMContext context;
+  llvm::Module module("synthtax_module", context);
+  llvm::IRBuilder<> builder(context);
+
+  synthtax_antlr::IRBuilder ir_builder(context, module, builder);
+  ir_builder.visitProg(prog);
+
+  std::string str;
+  llvm::raw_string_ostream os(str);
+  module.print(os, nullptr);
+
+  std::ofstream out;
+  out.open(filepath);
+  out << str;
+  out.close();
+}
+
 int main(int argc, const char *argv[]) {
 	if (argc != 3) {
 		std::cout << "Invalid argument(s)\n";
@@ -25,29 +53,13 @@ int main(int argc, const char *argv[]) {
   synthtax_antlr::SynthtaxParser parser(&tokens);
 	synthtax_antlr::SynthtaxParser::ProgContext *prog = parser.prog();
 
-  // synthtax_antlr::CppTranspilerVisitor visitor;
-  // visitor.visitProg(prog);
+  stream.close();
 
-  // std::ofstream out;
-  // out.open(argv[2]);
-  // out << visitor.outfile.str();
-  // out.close();
+  // Uncomment the line below to transpile to C++ code
+  // transpile(argv[2], prog);
 
-  llvm::LLVMContext context;
-  llvm::Module module("synthtax_module", context);
-  llvm::IRBuilder<> builder(context);
-
-  synthtax_antlr::IRBuilder ir_builder(context, module, builder);
-  ir_builder.visitProg(prog);
-
-  std::string str;
-  llvm::raw_string_ostream os(str);
-  module.print(os, nullptr);
-
-  std::ofstream out;
-  out.open(argv[2]);
-  out << str;
-  out.close();
+  // Output LLVM IR
+  build_ir(argv[2], prog);
 
   return 0;
 }
